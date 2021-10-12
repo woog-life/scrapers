3e67d170d650fc50023488c3cfcdebc5c9ca0586
Torben Carstens <git@carstens.tech>
2021-10-12 21:47:51 +0200

    Add CuxhavenSpider to main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from scrapy.crawler import CrawlerProcess
 
+from lake_scrapers.spiders.cuxhaven import CuxhavenSpider
 from lake_scrapers.spiders.pegelonline import PegelonlineSpider
 from lake_scrapers.spiders.woog import WoogSpider
 
 process = CrawlerProcess()
 process.crawl(PegelonlineSpider)
 process.crawl(WoogSpider)
+process.crawl(CuxhavenSpider)
 process.start()
