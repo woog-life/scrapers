9e190be7aa68454133cdbcfc0c18272019d2f0a3
Torben Carstens <git@carstens.tech>
2021-10-12 22:06:54 +0200

    Add AareSpider to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scrapy.crawler import CrawlerProcess
 
+from lake_scrapers.spiders.aare import AareSpider
 from lake_scrapers.spiders.alster import AlsterSpider
 from lake_scrapers.spiders.cuxhaven import CuxhavenSpider
 from lake_scrapers.spiders.pegelonline import PegelonlineSpider
@@ -10,4 +11,5 @@ process.crawl(PegelonlineSpider)
 process.crawl(WoogSpider)
 process.crawl(CuxhavenSpider)
 process.crawl(AlsterSpider)
+process.crawl(AareSpider)
 process.start()
