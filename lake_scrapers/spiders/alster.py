13a10cb21c439ab092058de8c5665f80a3951c5f
Torben Carstens <git@carstens.tech>
2021-10-12 22:05:39 +0200

    Fix alster time_format

--- lake_scrapers/spiders/alster.py
+++ lake_scrapers/spiders/alster.py
@@ -22,12 +22,12 @@ class AlsterSpider(scrapy.Spider):
             yield scrapy.Request(url, self.parse)
 
     def parse(self, response, **kwargs):
-        temp_xpath = "//td[contains(text(), 'Wassertemperatur')]/../td[3]//text()"
-        timestamp_xpath = "//td[contains(text(), 'Wassertemperatur')]/../td[1]//text()"
+        temp_xpath = "//td[contains(text(), 'Wassertemperatur')]/../td[3]/text()"
+        timestamp_xpath = "//td[contains(text(), 'Wassertemperatur')]/../td[1]/text()"
 
         temperature = response.xpath(temp_xpath).get().strip()
         timestamp = response.xpath(timestamp_xpath).get().strip()
-        timestamp = convert_timestamp(timestamp, time_format="%d.%m.%Y %H:%S")
+        timestamp = convert_timestamp(timestamp, time_format="%d.%m.%Y %H:%M")
 
         url = response.request.url
         key = url.rsplit("/")[-2]
