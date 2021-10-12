53cd1fae517fe8d537398247be95989c4fa19808
Torben Carstens <git@carstens.tech>
2021-10-12 20:24:47 +0200

    Add woog scraper

---
+++ lake_scrapers/spiders/woog.py
@@ -0,0 +1,32 @@
+import os
+
+import scrapy
+
+from lake_scrapers.items import LakeTemperatureItem
+from lake_scrapers.spiders.pegelonline import PegelonlineSpider
+
+
+class WoogSpider(scrapy.Spider):
+    data = {
+        "LQ8MXn": {
+            "UUID": os.getenv("LARGE_WOOG_UUID")
+        }
+    }
+    name = 'woog'
+    allowed_domains = ['woog.iot.service.itrm.de']
+    start_urls = ['https://woog.iot.service.itrm.de/?accesstoken=LQ8MXn']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, self.parse)
+
+    def parse(self, response, **kwargs):
+        temp_xpath = "Water_Temperature/value/text()"
+        time_xpath = "Water_Temperature/ts/text()"
+        temperature = response.xpath(temp_xpath).get()
+        timestamp = response.xpath(time_xpath).get()
+        url = response.request.url
+        accessToken = url.rsplit("=")[-1]
+        uuid = self.data[accessToken]["UUID"]
+
+        return LakeTemperatureItem(temperature=temperature, timestamp=timestamp, uuid=uuid)
