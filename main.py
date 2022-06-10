8f4dadaefa3567e00b159b293ceb867baa4a2571
Torben Carstens <git@carstens.tech>
2022-06-10 11:28:33 +0200

    Migrate to CrawlerRunner (+ scrapy 2.6.1)

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
 
 from lake_scrapers.spiders.aare import AareSpider
 from lake_scrapers.spiders.alster import AlsterSpider
 from lake_scrapers.spiders.cuxhaven import CuxhavenSpider
 from lake_scrapers.spiders.pegelonline import PegelonlineSpider
 from lake_scrapers.spiders.woog import WoogSpider
-from scrapy.utils.project import get_project_settings
 
-process = CrawlerProcess(get_project_settings())
-process.crawl(PegelonlineSpider)
-process.crawl(WoogSpider)
-process.crawl(CuxhavenSpider)
-process.crawl(AlsterSpider)
-process.crawl(AareSpider)
-process.start()
+crawlers = [PegelonlineSpider, WoogSpider, CuxhavenSpider, AlsterSpider, AareSpider]
+
+settings = get_project_settings()
+runner = CrawlerRunner(settings)
+for crawler in crawlers:
+    # process = CrawlerProcess(get_project_settings())
+    # process.crawl(crawler)
+    # process.start()
+    runner.crawl(crawler)
+
+d = runner.join()
+d.addBoth(lambda _: reactor.stop())
+reactor.run()
