dac3193bf4f6274e627d4d73895a5a8f9f788976
Torben Carstens <git@carstens.tech>
2023-09-13 20:12:56 +0200

    Fix seatemperature.info
    No `User-Agent` leads to a `403 Forbidden`, in general user-agents
    aren't explicitely disallowed by the robots.txt

--- lake_scrapers/spiders/seatemperatureinfo.py
+++ lake_scrapers/spiders/seatemperatureinfo.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import datetime
 
 import scrapy
@@ -11,11 +12,11 @@ class SeaTemperatureInfoSpider(scrapy.Spider):
     data = {
         "de/spanien/santander-wassertemperatur.html": {
             "UUID": os.getenv("SANTANDER_UUID"),
-            "xpath": "//tr[td='heute']/../tr[2]/td//text()"
+            "regex": re.compile(r"Die Wassertemperatur in Santander beträgt heute (?P<temperature>\d+(\.\d+)?)"),
         },
         "heraklion-water-temperature.html": {
             "UUID": os.getenv("HERAKLION_UUID"),
-            "xpath": "//tr[td='today temp']/../tr[2]/td//text()"
+            "regex": re.compile(r"Water temperature in Heraklion today is (?P<temperature>\d+(\.\d+)?)"),
         }
     }
     name = 'seatemperatureinfo'
@@ -25,13 +26,17 @@ class SeaTemperatureInfoSpider(scrapy.Spider):
     def start_requests(self):
         for path in self.data.keys():
             url = "/".join([self.base_url, path])
-            yield scrapy.Request(url, self.parse)
+            yield scrapy.Request(url, self.parse, headers={
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"
+            })
 
     def parse(self, response, **kwargs):
         url = response.request.url
         path = url.replace(self.base_url + "/", "")
-        temp_xpath = self.data[path]["xpath"]
-        temperature = response.xpath(temp_xpath).get()
+        regex = self.data[path]["regex"]
+        content: str = response.text
+        matches = regex.search(content)
+        temperature = matches.group("temperature")
 
         timestamp = datetime.now().timestamp()
         timestamp = convert_timestamp(timestamp, is_timestamp=True)
