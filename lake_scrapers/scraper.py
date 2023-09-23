ab200eec82290bad1678d69b0e00aa8578377c21
Torben Carstens <git@carstens.tech>
2023-09-23 16:44:14 +0200

    Formatting

--- lake_scrapers/scraper.py
+++ lake_scrapers/scraper.py
@@ -2,7 +2,6 @@ import urllib.robotparser
 from abc import abstractmethod
 from dataclasses import dataclass
 
-
 import bs4
 import httpx
 import lxml.etree
@@ -30,7 +29,7 @@ class Scraper:
     def soup(response: httpx.Response) -> bs4.BeautifulSoup:
         response.raise_for_status()
 
-        return bs4.BeautifulSoup(response.text, 'html.parser')
+        return bs4.BeautifulSoup(response.text, "html.parser")
 
     def request(self, path: str) -> httpx.Response:
         url = "/".join([self.base_url.rstrip("/"), path.lstrip("/")])
@@ -38,7 +37,8 @@ class Scraper:
         return httpx.get(url, headers=self.headers, timeout=15, follow_redirects=True)
 
     @abstractmethod
-    def parse(self, response: httpx.Response) -> LakeTemperatureItem: ...
+    def parse(self, response: httpx.Response) -> LakeTemperatureItem:
+        ...
 
     @staticmethod
     def xpath(xpath: str, response: httpx.Response, _type: str = "html"):
@@ -47,7 +47,9 @@ class Scraper:
         elif _type == "xml":
             tree: lxml.etree.Element = lxml.etree.XML(response.content)
         else:
-            raise NotImplementedError(f"this only supports 'html' and 'xml' not {_type}")
+            raise NotImplementedError(
+                f"this only supports 'html' and 'xml' not {_type}"
+            )
 
         return tree.xpath(xpath)
 
